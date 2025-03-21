46fb226074895c2aaf55d17c4c9de9c687a8593b
Anatoly Slabodenyuk <slabodenyukanatoly@gmail.com>
2025-03-21 12:40:18 +0300

    add art module

--- main.py
+++ main.py
@@ -3,9 +3,19 @@ from application.salary import calculate_salary
 
 from datetime import date
 
+from art import text2art
+
+
+def ascii_from_text(text):
+    ascii_art2 = text2art(text, font='slant')
+    print(ascii_art2)
+
+
 if __name__ == '__main__':
     calculate_salary(5, 3)
     print('________________________')
     get_employees()
     print('________________________')
     print(date.today())
+    print('________________________')
+    ascii_from_text("Python is great!")
